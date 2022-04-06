4f1538fe17054653bf7fa4007dbaf03ffe0e072c
ruslanguliyev <rguliyev27@gmail.com>
2022-04-06 18:51:15 +0400

    Добавьте файлы проекта.

---
+++ Bank switch/Helper/operation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_switch.Helper
+{
+    public class operation
+    {
+        public void PulDaxili(int limitD)
+        {
+            Console.WriteLine("Maksimal daxil edilen mebleg 500 AZN");
+            var daxilM = Console.ReadLine();
+
+            if(Convert.ToInt32(daxilM) > limitD)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pulunuzu geri gotutun");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else if (Convert.ToInt32(daxilM) < limitD)
+            {
+                Console.BackgroundColor= ConsoleColor.Green;
+                Console.WriteLine("Mebleg balansa daxil oldu");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+        public void PulCixarilishi(int limitC)
+        {
+            Console.WriteLine("Maksimal cixarish meblegi 200 AZN");
+            var CixM = Console.ReadLine();
+
+            if (Convert.ToInt32(CixM) > limitC)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Emeliyyat mumkun deyil");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else if(Convert.ToInt32(CixM) <= limitC)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Xais edirik pulunuzu goturun");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+
+    }
+}
---
+++ Bank switch/Program.cs
@@ -0,0 +1,63 @@
+﻿using Bank_switch.Helper;
+
+string name = "Ruslan";
+int cardNum = 123456789;
+int password = 1111;
+int balans = 500;
+int limitC = 200;
+int limitD = 500;
+
+
+basha:
+Console.WriteLine("Kart nomrenizi daxil edin:");
+var currentCard = Console.ReadLine();
+
+Console.WriteLine("PİN kodu daxil edin:");
+var currentPas = Console.ReadLine();
+
+
+
+if (cardNum == Convert.ToInt32(currentCard) && password == Convert.ToInt32(currentPas))   
+{
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine(name + "Bey" +  "Xosh geldiniz");
+   Console.BackgroundColor = ConsoleColor.Black;
+}
+else
+{
+    //(cardNum != Convert.ToInt32(currentCard) && password != Convert.ToInt32(currentPas));
+    Console.WriteLine("Login ve ya PIN yalnishdi");
+    goto basha;
+}
+
+
+evvele:
+Console.WriteLine(" Bir Emeliyyat Secin:");
+Console.WriteLine("1. Negd pulun daxil edilmesi");
+Console.WriteLine("2. Negd pulun cixarilmasi");
+Console.WriteLine("3. Balansi gostermek");
+Console.WriteLine("4. Karti qeri almaq");
+var action = Console.ReadLine();
+
+operation opr = new operation();    
+
+switch (action)
+{
+    case "1":
+        opr.PulDaxili(limitD);
+        goto evvele;
+        break;
+    case "2":
+        opr.PulCixarilishi(limitC);
+        goto evvele;
+        break;
+    case "3":
+        Console.WriteLine("Balansiniz:" + balans + "AZN");
+        goto evvele;
+        break;
+    case "4":
+        goto basha;
+        break;
+    default:
+        break;
+}
